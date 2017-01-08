2c19b12249f5c0d28cf2d065c321c453375015dc
Mohamed Sobeh <mqmsobeh@gmail.com>
2017-01-08 03:23:49 +0200

    More Adds

---
+++ Code_Helpers/System/Collections/PointerQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHelpers.System.Collections
+{
+	public class PointerQueue<T>
+	{
+
+		private class QueueNode<T>
+		{
+			private QueueNode<T> _firstNode;
+
+			public QueueNode<T> FirstNode
+			{
+				get { return _firstNode; }
+				set { _firstNode = value; }
+			}
+
+			private QueueNode<T> _lastNode;
+
+			public QueueNode<T> LastNode
+			{
+				get { return _lastNode; }
+				set { _lastNode = value; }
+			}
+
+		}
+	}
+}
